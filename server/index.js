43f2ceea8a02b688783f4240709b259ad8cf2a82
Prathap karaka <venkatasivaprathap1002@gmail.com>
2021-08-20 15:33:11 +0530

    user regisration page created and connected

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import Express from "express";
 import Dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 import Connection from "./database/db.js";
 import DefaultData from "./defaultData.js";
+import router from "./routes/routes.js";
 
 const app = Express();
+
+app.use(bodyParser.json({ extender: true }));
+app.use(bodyParser.urlencoded({ extender: true }));
+app.use(cors());
+app.use("/", router);
 
 const PORT = 8000;
 
